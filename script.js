b807ca6444ab98d08e3e5ea87ec9f093daf11b80
Sergio A. S <93699660+sergio-abu@users.noreply.github.com>
2022-06-11 00:45:08 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+function compute()
+{
+    //creating variables to perform operations
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value
+    var years = document.getElementById("years").value
+    var interest = principal * years * rate /100;
+    
+    var year = new Date().getFullYear()+parseInt(years);
+
+    //Conditional operation performed to check if the principle is valid or not
+    if(principal ==undefined || principal <=0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return
+    }
+    else{
+        var inr = "If you deposit <mark>" + principal+",</mark><br>";
+        var ra = "at an interest rate of <mark>" + rate+"%,</mark><br>";
+        var am = "You will receive an amount of <mark>"+ interest+",</mark><br>";
+        var ye = "in the year <mark'>" + year +"</mark>"; 
+        
+        document.getElementById("result").innerHTML=inr+ra+am+ye;
+    }
+
+}
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
